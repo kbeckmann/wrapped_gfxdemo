7bda1487def83f58dd45bf74cb937fbc6bb592e0
Konrad Beckmann <konrad.beckmann@gmail.com>
2021-06-11 00:44:40 +0200

    remove junk

--- myip1/src/myip1_gfxdemo.py
+++ myip1/src/myip1_gfxdemo.py
@@ -14,129 +14,6 @@ from pergola.gateware.vga2dvid import VGA2DVID
 from pergola.gateware.vga_testimage import TestImageGenerator, RotozoomImageGenerator
 
 
-
-
-class DVIDParameters():
-    def __init__(self, vga_parameters, pll1_freq_mhz, pixel_freq_mhz):
-        self.vga_parameters = vga_parameters
-        self.pll1_freq_mhz = pll1_freq_mhz
-        self.pixel_freq_mhz = pixel_freq_mhz
-
-    def __repr__(self):
-        return "(DVIDParameters ({}) {})".format(
-            self.vga_parameters,
-            self.pll1_freq_mhz,
-            self.pixel_freq_mhz)
-
-dvid_configs = {
-    "640x480p60": DVIDParameters(VGAParameters(
-            h_front=16,
-            h_sync=96,
-            h_back=44,
-            h_active=640,
-            v_front=10,
-            v_sync=2,
-            v_back=31,
-            v_active=480,
-        ), 100, 25),
-
-    # This uses a clock that is compatible with xdr=7
-    "640x480p60_7": DVIDParameters(VGAParameters(
-            h_front=16,
-            h_sync=96,
-            h_back=44,
-            h_active=640,
-            v_front=10,
-            v_sync=2,
-            v_back=31,
-            v_active=480,
-        ), 100, 28),
-
-    "1280x720p60": DVIDParameters(VGAParameters(
-            h_front=82,
-            h_sync=80,
-            h_back=202,
-            h_active=1280,
-            v_front=3,
-            v_sync=5,
-            v_back=22,
-            v_active=720,
-        ), 100, 74),
-
-    # This uses a clock that is compatible with xdr=7
-    "1280x720p60_7": DVIDParameters(VGAParameters(
-            h_front=82,
-            h_sync=80,
-            h_back=202,
-            h_active=1280,
-            v_front=3,
-            v_sync=5,
-            v_back=22,
-            v_active=720,
-        ), 100, 70),
-
-    "1920x1080p30": DVIDParameters(VGAParameters(
-            h_front=80,
-            h_sync=44,
-            h_back=148,
-            h_active=1920,
-            v_front=4,
-            v_sync=5,
-            v_back=36,
-            v_active=1080,
-        ), 100, 74),
-
-    # This uses a clock that is compatible with xdr=7
-    "1920x1080p30_7": DVIDParameters(VGAParameters(
-            h_front=100,
-            h_sync=44,
-            h_back=215,
-            h_active=1920,
-            v_front=4,
-            v_sync=5,
-            v_back=36,
-            v_active=1080,
-        ), 100, 77),
-
-    # Should be 148.5 MHz but the PLL can't generate 742.5 MHz.
-    "1920x1080p60": DVIDParameters(VGAParameters(
-            h_front=88,
-            h_sync=44,
-            h_back=148,
-            h_active=1920,
-            v_front=4,
-            v_sync=5,
-            v_back=36,
-            v_active=1080,
-        ), 100, 150),
-
-    "2560x1440p30": DVIDParameters(VGAParameters(
-            h_front=48,
-            h_sync=32,
-            h_back=80,
-            h_active=2560,
-            v_front=3,
-            v_sync=5,
-            v_back=33,
-            v_active=1440,
-        ), 100, 122),
-
-    # Generates a 60Hz signal but needs 1.2V on VCC.
-    # Needs a simpler test image to meet timing on the sync/pixel cd.
-    # Can run at 205MHz@1.1V
-    "2560x1440p60": DVIDParameters(VGAParameters(
-            h_front=20,
-            h_sync=20,
-            h_back=20,
-            h_active=2560,
-            v_front=3,
-            v_sync=5,
-            v_back=5,
-            v_active=1440,
-        ), 100, 228),
-}
-
-
 if __name__ == "__main__":
     parser = main_parser()
     args = parser.parse_args()
@@ -156,7 +33,7 @@ if __name__ == "__main__":
     m.domains += ClockDomain("shift")
     m.submodules += ResetSynchronizer(reset, domain="shift")
 
-    dvid_config = dvid_configs["640x480p60"]
+    dvid_config = GFXDemo.dvid_configs["640x480p60"]
 
     pdm_out = Signal()
     m.submodules.gfxdemo = gfxdemo = GFXDemo(
