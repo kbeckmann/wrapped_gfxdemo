183bac0b73b9095eb94299cde07832bbd853ede0
Konrad Beckmann <konrad.beckmann@gmail.com>
2021-06-10 21:51:38 +0200

    Add config to handle second clock domain

--- config.tcl
+++ config.tcl
@@ -7,7 +7,7 @@ set ::env(DESIGN_NAME) wrapped_myip1
 set ::env(VERILOG_FILES) "$::env(DESIGN_DIR)/wrapper.v \
     $::env(DESIGN_DIR)/myip1/src/myip1.v"
 
-set ::env(PL_TARGET_DENSITY) 0.4
+set ::env(PL_TARGET_DENSITY) 0.50
 set ::env(DIE_AREA) "0 0 300 300"
 set ::env(FP_SIZING) absolute
 
@@ -15,11 +15,15 @@ set ::env(SYNTH_DEFINES) "MPRJ_IO_PADS=38"
 
 # Fill this
 set ::env(CLOCK_PERIOD) "1"
-set ::env(CLOCK_PORT) "user_clock2"
+set ::env(CLOCK_PORT) "wb_clk_i"
+# set ::env(CLOCK_PORT) "user_clock2"
+
+#set ::env(CLOCK_NET) "wb_clk_i"
 
 set ::env(DESIGN_IS_CORE) 0
 set ::env(GLB_RT_MAXLAYER) 5
 
+# set ::env(DIODE_INSERTION_STRATEGY) "3"
 
 
 set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
@@ -28,3 +32,23 @@ set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
 set ::env(RUN_CVC) 0
+
+# stop OpenLANE from inserting clock buffers, makes tristate problematic
+set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
+
+
+# ******************************************************************
+# ** Code for shimming SDC file to allow for 2nd clock constraint **
+# ******************************************************************
+# clock2 period is ns
+set ::env(CLOCK2_PERIOD) "5"
+set ::env(CLOCK2_PORT) "user_clock2"
+
+set ::env(BASE_SDC_FILE_SHIM) "$::env(DESIGN_DIR)/shim.sdc"
+
+# for some reason this gets sourced multiple times so we need to grab original base.sdc only when it doesn't match our shim
+if {! [string match $::env(BASE_SDC_FILE) $::env(BASE_SDC_FILE_SHIM)]} {
+    set ::env(BASE_SDC_FILE_OLD) $::env(BASE_SDC_FILE)
+}
+
+set ::env(BASE_SDC_FILE) $::env(BASE_SDC_FILE_SHIM)
