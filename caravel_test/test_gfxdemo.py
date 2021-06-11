b3bd701c6a878bfe8f9745fc469ca09098ca8497
Konrad Beckmann <konrad.beckmann@gmail.com>
2021-06-11 10:37:43 +0200

    Remove dummy assert

--- caravel_test/test_gfxdemo.py
+++ caravel_test/test_gfxdemo.py
@@ -4,23 +4,23 @@ from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles
 
 @cocotb.test()
 async def test_start(dut):
-    clock = Clock(dut.clk, 25, units="ns") # 40M
+    clock = Clock(dut.clk, 40, units="ns")
     cocotb.fork(clock.start())
     
     dut.RSTB <= 0
-    dut.power1 <= 0;
-    dut.power2 <= 0;
-    dut.power3 <= 0;
-    dut.power4 <= 0;
+    dut.power1 <= 0
+    dut.power2 <= 0
+    dut.power3 <= 0
+    dut.power4 <= 0
 
     await ClockCycles(dut.clk, 8)
-    dut.power1 <= 1;
+    dut.power1 <= 1
     await ClockCycles(dut.clk, 8)
-    dut.power2 <= 1;
+    dut.power2 <= 1
     await ClockCycles(dut.clk, 8)
-    dut.power3 <= 1;
+    dut.power3 <= 1
     await ClockCycles(dut.clk, 8)
-    dut.power4 <= 1;
+    dut.power4 <= 1
 
     await ClockCycles(dut.clk, 80)
     dut.RSTB <= 1
@@ -31,51 +31,7 @@ async def test_start(dut):
     # wait for reset to go low
     await FallingEdge(dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.reset)
 
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc0_a <= 0
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc1_a <= 0
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc2_a <= 0
-    # await ClockCycles(dut.clk, 2)
-
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc0_a <= 1
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc1_a <= 0
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc2_a <= 0
-    # await ClockCycles(dut.clk, 2)
-
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc0_a <= 0
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc1_a <= 1
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc2_a <= 0
-    # await ClockCycles(dut.clk, 2)
-
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc0_a <= 1
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc1_a <= 1
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc2_a <= 0
-    # await ClockCycles(dut.clk, 2)
-
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc0_a <= 0
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc1_a <= 0
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc2_a <= 1
-    # await ClockCycles(dut.clk, 2)
-
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc0_a <= 1
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc1_a <= 0
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc2_a <= 1
-    # await ClockCycles(dut.clk, 2)
-
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc0_a <= 0
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc1_a <= 1
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc2_a <= 1
-    # await ClockCycles(dut.clk, 2)
-
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc0_a <= 1
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc1_a <= 1
-    # dut.uut.mprj.wrapped_gfxdemo.gfxdemo_0.enc2_a <= 1
-    # await ClockCycles(dut.clk, 2)
-
-
-
     # wait
     await ClockCycles(dut.clk, 6000)
 
-    # assert something
-    assert(0 == 25)
 
