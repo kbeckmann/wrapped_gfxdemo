ab14a334df909ead0ad41b868efa2b8ed7ac74dd
Konrad Beckmann <konrad.beckmann@gmail.com>
2021-06-11 11:11:15 +0200

    Minimal test case

--- gfxdemo/test/dump_gfxdemo.v
+++ gfxdemo/test/dump_gfxdemo.v
@@ -1,3 +1,21 @@
+`default_nettype none
+
+`timescale 1 ns / 1 ps
+
+module gfxdemo_tb ();
+
+    wire clk;
+    wire clk_shift;
+    wire rst;
+
+    gfxdemo gfxdemo_0(
+        .clk        (clk),
+        .reset      (rst),
+        .shift_clk  (shift_clk)
+    );
+
+endmodule
+
 module dump();
     initial begin
         $dumpfile ("gfxdemo.vcd");
--- gfxdemo/test/test_gfxdemo.py
+++ gfxdemo/test/test_gfxdemo.py
@@ -2,27 +2,17 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles
 
-clocks_per_phase = 10
-
-async def reset(dut):
-    dut.enc0_a <= 0
-    dut.enc0_b <= 0
-    dut.enc1_a <= 0
-    dut.enc1_b <= 0
-    dut.enc2_a <= 0
-    dut.enc2_b <= 0
-    dut.reset  <= 1
-
-    await ClockCycles(dut.clk, 5)
-    dut.reset <= 0
-    await ClockCycles(dut.clk, 5)
-
 @cocotb.test()
-async def test_all(dut):
-    clock = Clock(dut.clk, 10, units="us")
-
+async def test_start(dut):
+    clock = Clock(dut.clk, 40, units="ns")
     cocotb.fork(clock.start())
 
-    await ClockCycles(dut.clk, 2)
+    await ClockCycles(dut.clk, 10)
+    dut.reset <= 1
 
-    await ClockCycles(dut.clk, 200)
+    await ClockCycles(dut.clk, 10)
+    dut.reset <= 0
+
+    # wait
+    await ClockCycles(dut.clk, 6000)
+
